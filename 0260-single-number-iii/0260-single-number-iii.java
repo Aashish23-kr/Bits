2c00889fefb5e3bde086fd27ffa60bcfabc71afe
Aashish Kumar <aashishkumar44651@gmail.com>
2026-08-05 18:51:03 +0530

    Time: 1 ms (100%), Space: 48.3 MB (9.28%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        long xor =0;
+
+        for(int num : nums){
+            xor ^= num;
+        }
+        int mask = (int) (xor & -xor);
+
+        int groupa =0;
+        int groupb =0;
+
+        for(int num : nums){
+            if((num & mask) != 0){
+                groupa ^= num;
+            }else{
+                groupb ^= num;
+            }
+        }
+        return new int[]{groupa,groupb};
+    }
+}
